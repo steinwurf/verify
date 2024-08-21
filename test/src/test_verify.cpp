379abadbe5562c71f21e91c9389caa875b3a37d9
Sofie Finnes Øvrelid <sofie+git@steinwurf.com>
2024-08-21 09:27:18 +0200

    chore: clang format

--- test/src/test_verify.cpp
+++ test/src/test_verify.cpp
@@ -1,19 +1,18 @@
+#include <gtest/gtest.h>
 #include <map>
 #include <string>
-#include <gtest/gtest.h>
 #include <verify.hpp>
 
-TEST(test_verify, simple)
-{
-    int a = 42;
-    VERIFY(a == 42);
-    VERIFY(a = 21);
-    VERIFY(a == 21);
+TEST(test_verify, simple) {
+  int a = 42;
+  VERIFY(a == 42);
+  VERIFY(a = 21);
+  VERIFY(a == 21);
 }
 
-TEST(test_verify, complex)
-{
+TEST(test_verify, complex) {
   std::map<std::string, int> example_map{{"test", 1}, {"foo", 2}};
   auto it = example_map.find("foo");
-  VERIFY(it != example_map.cend(), "expected key not found", example_map, "hello", "world");
+  VERIFY(it != example_map.cend(), "expected key not found", example_map,
+         "hello", "world");
 }
