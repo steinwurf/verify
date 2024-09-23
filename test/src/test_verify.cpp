0ed8fe73d5efcb082f2ace33d76f2b1f2f9132ee
Sofie Finnes Øvrelid <sofie+git@steinwurf.com>
2024-09-23 13:40:01 +0200

    chore: clang format

--- test/src/test_verify.cpp
+++ test/src/test_verify.cpp
@@ -8,7 +8,8 @@ TEST(test_verify, simple) {
   VERIFY(a == 42);
   VERIFY(a = 21);
   VERIFY(a == 21);
-  (void)a; // Make compilers happy when the variable goes unused when VERIFY is compiled out.
+  (void)a; // Make compilers happy when the variable goes unused when VERIFY is
+           // compiled out.
 }
 
 TEST(test_verify, complex) {
@@ -16,7 +17,8 @@ TEST(test_verify, complex) {
   auto it = example_map.find("foo");
   VERIFY(it != example_map.cend(), "expected key not found", example_map,
          "hello", "world");
-  (void)it; // Make compilers happy when the variable goes unused when VERIFY is compiled out.
+  (void)it; // Make compilers happy when the variable goes unused when VERIFY is
+            // compiled out.
 }
 
 void trigger_verify() {
@@ -24,7 +26,8 @@ void trigger_verify() {
   auto it = example_map.find("bar");
   VERIFY(it != example_map.cend(), "expected key not found", example_map,
          "hello", "world");
-  (void)it; // Make compilers happy when the variable goes unused when VERIFY is compiled out.
+  (void)it; // Make compilers happy when the variable goes unused when VERIFY is
+            // compiled out.
 }
 
 TEST(test_verify, failing) {
