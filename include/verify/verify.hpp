305211a0312311cd4fc7a0ca1fd660d5891486cc
Morten V. Pedersen <morten@mortenvp.com>
2025-05-25 09:40:24 +0200

    CMake update

--- include/verify/verify.hpp
+++ include/verify/verify.hpp
@@ -5,23 +5,6 @@
 
 #pragma once
 
-// #ifdef NDEBUG
-// #define VERIFY(...) ((void)0)
-// #else
-// // Select the appropriate backend based on the C++ standard and
-// configuration. #ifdef STEINWURF_VERIFY_USE_LIBASSERT #include
-// "backends/libassert.hpp" #else #include "backends/assert.hpp" #endif //
-// STEINWURF_VERIFY_USE_LIBASSERT
-
-// #ifdef _MSC_VER
-// #define VERIFY VERIFY_IMPL
-// #else
-// #define VERIFY(...) VERIFY_IMPL(__VA_ARGS__)
-// #endif // _MSC_VER
-// #endif // NDEBUG
-
-////////////////////////////////////
-
 #pragma once
 
 #ifdef STEINWURF_LIBASSERT
