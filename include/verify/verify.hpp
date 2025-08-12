d841c760eda2949ac4b3c8cc4546ca809f7153eb
Jeppe Axelsen <jeppebergaxelsen@gmail.com>
2025-08-12 14:57:09 +0200

    refactor(verify): enhance VERIFY macro for better argument handling
    * Introduced VERIFY1 and VERIFY2 macros to improve argument handling.
    * Updated VERIFY_DISPATCH to support up to 64 user arguments.
    * Ensured proper parenthesis usage to avoid precedence warnings.

--- include/verify/verify.hpp
+++ include/verify/verify.hpp
@@ -15,8 +15,35 @@
 #define EXPAND(x) x
 
 #define DEBUG_VERIFY(...) EXPAND(LIBASSERT_DEBUG_ASSERT(__VA_ARGS__))
-#define VERIFY(expr, ...) EXPAND(LIBASSERT_ASSERT((expr) , ##__VA_ARGS__))
+// Keep expr parenthesized to avoid precedence warnings
+#define VERIFY1(expr) EXPAND(LIBASSERT_ASSERT((expr)))
+#define VERIFY2(expr, ...) EXPAND(LIBASSERT_ASSERT((expr), __VA_ARGS__))
 
+// Pick the 65th parameter; supports up to 64 user arguments (bump if needed)
+#define VERIFY_GET_MACRO(                                                      \
+    _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16,     \
+    _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, \
+    _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, \
+    _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, \
+    _62, _63, _64, NAME, ...)                                                  \
+    NAME
+
+#define VERIFY_DISPATCH(...)                                           \
+    VERIFY_GET_MACRO(                                                  \
+        __VA_ARGS__, /* 64 times VERIFY2 */                            \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, VERIFY2, \
+        VERIFY2, /* fallback when only 1 arg */                        \
+        VERIFY1)
+
+#define VERIFY(...) EXPAND(VERIFY_DISPATCH(__VA_ARGS__)(__VA_ARGS__))
 
 #else
 
